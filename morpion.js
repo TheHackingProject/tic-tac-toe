659b1719eb8ffd8f53e27b940f265daebce1a82f
Paul Maillardet <paul@pulsanova.com>
2020-12-30 00:44:06 +0100

    Improve refactoring

--- morpion.js
+++ morpion.js
@@ -18,20 +18,14 @@ class Morpion {
 		this.gridMap.forEach((line, y) => {
 			line.forEach((cell, x) => {
 				this.getCell(x, y).onclick = () => {
-					this.playerTurn(x, y);
+					this.doPlayHuman(x, y);
 				};
 			});
 		});
 
 		if (this.iaPlayer === 'J1') {
-			this.iaTurn();
+			this.doPlayIa();
 		}
-	}
-
-	displayEndMessage = (message) => {
-		const endMessageElement = document.getElementById('end-message');
-		endMessageElement.textContent = message;
-		endMessageElement.style.display = 'block';
 	}
 
 	getCell = (x, y) => {
@@ -39,6 +33,70 @@ class Morpion {
 		const lines = ['A', 'B', 'C'];
 		const cellId = `${lines[y]}${column}`;
 		return document.getElementById(cellId);
+	}
+
+    getBoardWinner = (board) => {
+        const isWinningRow = ([a, b, c]) => (
+            a !== null && a === b && b === c
+        );
+
+        let winner = null;
+
+        // Horizontal
+        board.forEach((line) => {
+            if (isWinningRow(line)) {
+                winner = line[0];
+            }
+        });
+
+        // Vertical
+        [0, 1, 2].forEach((col) => {
+            if (isWinningRow([board[0][col], board[1][col], board[2][col]])) {
+                winner = board[0][col];
+            }
+        });
+
+        if (winner) {
+            return winner;
+        }
+
+        // Diagonal
+        const diagonal1 = [board[0][0], board[1][1], board[2][2]];
+        const diagonal2 = [board[0][2], board[1][1], board[2][0]];
+        if (isWinningRow(diagonal1) || isWinningRow(diagonal2)) {
+            return board[1][1];
+        }
+
+        const isFull = board.every((line) => (
+			line.every((cell) => cell !== null)
+		));
+        return isFull ? 'tie' : null;
+    }
+
+	checkWinner = (lastPlayer) => {
+        const winner = this.getBoardWinner(this.gridMap);
+        if (!winner) {
+            return;
+        }
+
+        this.gameOver = true;
+        switch(winner) {
+            case 'tie':
+			    this.displayEndMessage("Vous êtes à égalité !");
+                break;
+            case this.iaPlayer:
+                this.displayEndMessage("L'IA a gagné !");
+                break;
+            case this.humanPlayer:
+                this.displayEndMessage("Tu as battu l'IA !");
+                break;
+        }
+	}
+
+	displayEndMessage = (message) => {
+		const endMessageElement = document.getElementById('end-message');
+		endMessageElement.textContent = message;
+		endMessageElement.style.display = 'block';
 	}
 
 	drawHit = (x, y, player) => {
@@ -52,60 +110,17 @@ class Morpion {
 		return true;
 	}
 
-	isFull = () => {
-		return this.gridMap.every((line) => (
-			line.every((cell) => cell !== null)
-		));
-	}
-
-	checkWinner = (lastPlayer) => {
-		const one = this.gridMap[0][0];
-		const two = this.gridMap[0][1];
-		const three = this.gridMap[0][2];
-		const four = this.gridMap[1][0];
-		const five = this.gridMap[1][1];
-		const six = this.gridMap[1][2];
-		const seven = this.gridMap[2][0];
-		const eight = this.gridMap[2][1];
-		const nine = this.gridMap[2][2];
-
-		if (one === two && one === three && one !== null ||
-			four === five && four === six && four !== null ||
-			seven === eight && seven === nine && seven !== null ||
-			one === five && one === nine && one !== null ||
-			three === five && three === seven && three !== null ||
-			one === four && one === seven && one !== null ||
-			two === five && two === eight && two !== null ||
-			three === six && three === nine && three !== null
-		) {
-			this.gameOver = true;
-			if (lastPlayer === this.iaPlayer) {
-				this.displayEndMessage("L'IA a gagné !");
-				return;
-			}
-
-			if (lastPlayer === this.humanPlayer) {
-				this.displayEndMessage("Tu as battu l'IA !");
-				return;
-			}
-		} else if (this.isFull()) {
-			this.gameOver = true;
-			this.displayEndMessage("Vous êtes à égalité !");
-			return;
-		}
-	}
-
-	playerTurn = (x, y) => {
+	doPlayHuman = (x, y) => {
 		if (this.gameOver) {
 			return;
 		}
 
 		if (this.drawHit(x, y, this.humanPlayer)) {
-			this.iaTurn();
+			this.doPlayIa();
 		}
 	}
 
-	iaTurn = () => {
+	doPlayIa = () => {
 		if (this.gameOver) {
 			return;
 		}
@@ -120,5 +135,3 @@ class Morpion {
 		});
 	}
 }
-
-const morpion = new Morpion();
